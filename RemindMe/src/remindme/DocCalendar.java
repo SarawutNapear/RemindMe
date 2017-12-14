eebeeedc9d9a99239baecaef1ba3f6db8bd22416
SarawutNapear <Sarawut.Napear@gmail.com>
2017-12-14 13:19:14 +0700

    Create DocCalendar

---
+++ RemindMe/src/remindme/DocCalendar.java
@@ -0,0 +1,43 @@
+package remindme;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class DocCalendar {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DocCalendar window = new DocCalendar();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DocCalendar() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
